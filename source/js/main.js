69891e2b99a68e303da061721cb81f9c35e8a9d3
nikita hlamov <misa1maho9x2.faires5love@gmail.com>
2024-02-27 23:48:48 +0300

    добавляет базу, хэдер, футер

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,8 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {CustomSelect} from './modules/select/custom-select';
 import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
+import {Burger} from './modules/header/burger';
+import {StickyHeader} from './modules/header/sticky-header';
 
 // ---------------------------------
 
@@ -19,6 +21,10 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    const burger = new Burger();
+    burger.init();
+    const stickyHeader = new StickyHeader();
+    stickyHeader.init();
     initModals();
     uploadFile();
     uploadImageDrop();
